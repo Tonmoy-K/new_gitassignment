fb8b16cd11107745acba72c3e97182f50c0340c1
Tonmoy-K <www.tkkt123@gmail.com>
2020-07-01 10:46:25 +0530

    mult-add feature added in calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,18 @@
 def calculator():
     print("Hi I am empty calculator")
+
+def multiplication(a,b):
+    return a*b
+
+def addition(a,b):
+    return a+b
+
+mult = multiplication(num1,num2)
+add = addition(num1,num2)
+
+num1 = 10
+num2 = 20
+
+print("multiplication = ",mult)
+print("addition = ",add)
+
